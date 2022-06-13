db89d65d54b2212c1f4fdea2e491f3a526b42003
Andrei Osadchii <nc1xo099@gmail.com>
2022-06-13 21:51:23 +0400

    fix domnesting

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,6 @@ import React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Button from "@mui/material/Button";
 import { NavLink } from "react-router-dom";
 import { Link } from "@mui/material";
 
@@ -11,26 +10,45 @@ function Navbar() {
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="sticky" sx={{ boxShadow: 0 }}>
         <Toolbar>
-          <NavLink to="/">
-            <Link color="error" underline="hover" sx={{ mr: "2rem" }}>
-              Home
-            </Link>
-          </NavLink>
-          <NavLink to="todolist">
-            <Link color="error" underline="hover" sx={{ mr: "2rem" }}>
-              ToDo
-            </Link>
-          </NavLink>
-          <NavLink to="chess">
-            <Link color="error" underline="hover" sx={{ mr: "2rem" }}>
-              Chess
-            </Link>
-          </NavLink>
-          <NavLink to="chat">
-            <Link color="error" underline="hover" sx={{ mr: "2rem" }}>
-              Chat
-            </Link>
-          </NavLink>
+          <Link
+            component={NavLink}
+            to="/"
+            color="error"
+            underline="hover"
+            sx={{ mr: "2rem" }}
+          >
+            Home
+          </Link>
+
+          <Link
+            component={NavLink}
+            to="todolist"
+            color="error"
+            underline="hover"
+            sx={{ mr: "2rem" }}
+          >
+            ToDo
+          </Link>
+
+          <Link
+            component={NavLink}
+            to="chess"
+            color="error"
+            underline="hover"
+            sx={{ mr: "2rem" }}
+          >
+            Chess
+          </Link>
+
+          <Link
+            component={NavLink}
+            to="chat"
+            color="error"
+            underline="hover"
+            sx={{ mr: "2rem" }}
+          >
+            Chat
+          </Link>
         </Toolbar>
       </AppBar>
     </Box>
