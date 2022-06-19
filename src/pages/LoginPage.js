ae8902088dc638d7d0c8192b63673a8996ad26b5
Andrei Osadchii <nc1xo099@gmail.com>
2022-06-19 16:24:45 +0400

    add link to login/register

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,12 +1,28 @@
+import { Box, Container, Link, Typography } from "@mui/material";
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Link as RouterLink } from "react-router-dom";
 import Login from "../components/auth/Login";
 
 function LoginPage() {
-  const location = useLocation();
-  const fromPage = location.state?.from?.pathname || "/";
-
-  return <Login />;
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Login />
+      <Box sx={{ alignSelf: "center", marginTop: "1.5rem" }}>
+        <Typography variant="body1">
+          Don't have an account yet?{" "}
+          <Link component={RouterLink} to={"/register"}>
+            Sign up
+          </Link>
+        </Typography>
+      </Box>
+    </Container>
+  );
 }
 
 export default LoginPage;
--- src/pages/RegistrationPage.js
+++ src/pages/RegistrationPage.js
@@ -1,8 +1,28 @@
+import { Box, Container, Link, Typography } from "@mui/material";
 import React from "react";
+import { Link as RouterLink } from "react-router-dom";
 import Register from "../components/auth/Register";
 
 function RegistrationPage() {
-  return <Register />;
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <Register />
+      <Box sx={{ alignSelf: "center", marginTop: "1.5rem" }}>
+        <Typography variant="body1">
+          Already have an account?{" "}
+          <Link component={RouterLink} to={"/login"}>
+            Sign in
+          </Link>
+        </Typography>
+      </Box>
+    </Container>
+  );
 }
 
 export default RegistrationPage;
